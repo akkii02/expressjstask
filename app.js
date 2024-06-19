37263cf6b266e791404b4d292b9f176c6557df9a
akshaysable02 <akshaysable097@gmail.com>
2024-06-19 16:20:12 +0530

    Intro to Middlewares  Task 2

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const http = require('http');
-const express = require('express')
-const app = express();
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use((req,res,next)=>{
-      console.log("In the Middleware")
-      next()
-})
-app.use((req,res,next)=>{
-      console.log("In another Middleware")
-      res.send('<h1>Hello Akshay</h1>')
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<html><head><title>task 2</title></head><body><form action="/product" method="POST" ><input type="text" name="title" /><input type="number" name="quantity" /><button type="submit">Add Product</button></form></body></html>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Akshay</h1>");
+});
 
 const server = http.createServer(app);
 
