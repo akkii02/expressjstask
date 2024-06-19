4deaaf94c2a1b65a9ea5757530387adac82b6724
akshaysable02 <akshaysable097@gmail.com>
2024-06-19 17:46:16 +0530

    Routers and Filters Task 3

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const http = require("http");
 const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const app = express();
 const bodyParser = require("body-parser");
 
-const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<html><head><title>task 2</title></head><body><form action="/product" method="POST" ><input type="text" name="title" /><input type="number" name="quantity" /><button type="submit">Add Product</button></form></body></html>'
-  );
+app.use("/admin",adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello Akshay</h1>");
-});
-
 const server = http.createServer(app);
 
 server.listen(3000);
